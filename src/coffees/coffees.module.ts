4cb30cdda292821ff2eab4585c9afba05cdcc6d0
mayang <569760498@qq.com>
2022-08-17 21:57:36 +0800

    p24

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeEntity } from './entites/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CoffeeEntity])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
