e4a8952b71a7b0ebb869f67252a8bd6e19272e48
mayang <569760498@qq.com>
2022-08-18 22:47:01 +0800

    p40 async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,25 +6,21 @@ import { Coffee } from './entites/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
-
-@Injectable()
-export class CoffeeBrandsFactory {
-  create() {
-    return ['buddy brew', 'nescafe'];
-  }
-}
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-        brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query()
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('[!] Async factory');
+        return coffeeBrands;
+      },
     },
   ],
   exports: [CoffeesService],
