fd7792b197dc2ce0a8b028fd81cfa55e182edb44
mayang <569760498@qq.com>
2022-08-20 17:42:36 +0800

    p65 add tags

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -16,11 +16,14 @@ import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { Protocol } from '../common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeeService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   @Get()
   findAll(
     @Protocol('https') protocol: string,
